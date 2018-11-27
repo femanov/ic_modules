5070a0de729dce531ba7a0779604e27738a5997b
femanov <femanov@gmail.com>
2018-11-27 14:34:09 +0700

    ddm update, just-show CXLineEdit implemented

--- fwidgets/cx_lineedit.py
+++ fwidgets/cx_lineedit.py
@@ -1,9 +1,21 @@
-from aQt.QtWidrets import QLineEdit
-
-
+from aQt.QtWidgets import QLineEdit
+import pycx4.qcda as cda
 
 class CXLineEdit(QLineEdit):
     def __init__(self, *args, **kwargs):
         super(CXLineEdit, self).__init__(*args)
         self._cname = kwargs.get('cname', None)
+        self.setReadOnly(bool(kwargs.get('readonly', False)))
+        self.chan = None
+        self.cx_connect()
 
+    def cx_connect(self):
+        if self._cname is None:
+            return
+        self.chan = cda.StrChan(self._cname, private=True, on_update=True)
+        self.chan.valueChanged.connect(self.cs_update)
+
+
+    def cs_update(self, chan):
+        if self.text() != chan.val:
+            self.setText(chan.val)
