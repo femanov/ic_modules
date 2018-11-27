4a8179f38d659727124e60ab6ad39fdc01952018
femanov <femanov@gmail.com>
2018-11-27 11:41:57 +0700

    ddm demonization and icons for combobox

--- fwidgets/cx_combobox.py
+++ fwidgets/cx_combobox.py
@@ -1,4 +1,5 @@
 from aQt.QtWidgets import QComboBox
+from aQt.QtGui import QIcon
 from aQt.QtCore import pyqtSlot, pyqtProperty
 import pycx4.qcda as cda
 
@@ -8,7 +9,7 @@ class CXTextComboBox(QComboBox):
         super(CXTextComboBox, self).__init__(*args)
         self._cname = kwargs.get('cname', None)
         self._values = ['None'] + kwargs.get('values', [])
-
+        self._icons = kwargs.get('icons', None)
 
         self.chan = None
         self.cx_connect()
@@ -16,6 +17,8 @@ class CXTextComboBox(QComboBox):
 
         for x in range(len(self._values)):
             self.insertItem(x, self._values[x])
+            if self._icons is not None and x > 0:
+                self.setItemIcon(x, QIcon(self._icons[x-1]))
 
         self.model().item(0).setEnabled(False)
 
